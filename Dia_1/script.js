d4e23dcbab7f71238be62ac02fe81fad00aaec7e
Juan Rivero <juandariver1@outlook.com>
2024-04-11 09:10:50 -0500

    feat: :sparkles: Creacion del archivo js

---
+++ Dia_1/script.js
@@ -0,0 +1,35 @@
+// ** FUNCION SIN RETORNO Y SIN PARAMETROS **
+function funcionNormal(){
+    console.log("Mi función");
+}
+
+// ** FUNCION SIN RETORNO Y CON PARAMETROS **
+function suma(a,b){
+    console.log(a+b);
+}
+// ** FUNCION CON RETORNO Y CON PARAMETROS **
+function sumaR(a,b){
+    //console.log(a+b);
+    return a+b;
+}
+// ** FUNCION CON RETORNO Y SIN PARAMETROS **
+function salonFavorito(){
+    //console.log(a+b);
+    return "P1";
+}
+
+
+
+// ************************************************ BUCLE FOR ************************************************
+
+
+function convertir(Celsius){
+    Fahrenheit = (((9*Celsius)/5) + 32);
+    return Fahrenheit;
+}
+
+let Celsius = 20;
+console.log(convertir(Celsius));
+
+
+
